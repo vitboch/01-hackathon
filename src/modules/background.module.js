e37a93af0cf77b468ae37798dfdd3fc67e73706a
LiliyaShaih <shayhelis@yandex.ru>
2022-08-27 15:44:14 +0300

    feature random background

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,36 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
+import { randomColor } from '../utils';
 
 export class BackgroundModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+  }
 
+  trigger() {
+    this.createHtml();
+    const backgroundModule = document.querySelector('.background-module');
+    backgroundModule.classList.add('open');
+
+    const colorBtn = document.querySelector('.background-module__btn');
+    colorBtn.addEventListener('click', this.getRandomColor);
+  }
+
+  getRandomColor() {
+    const firstColor = randomColor();
+    const secondColor = randomColor();
+    document.body.style.background = `linear-gradient(${firstColor},${secondColor})`;
+  }
+
+  createHtml() {
+    const backgroundModule = document.createElement('div');
+    backgroundModule.classList.add('background-module');
+
+    const backgroundButton = document.createElement('button');
+    backgroundButton.classList.add('background-module__btn');
+    backgroundButton.textContent = 'Change background';
+    backgroundButton.type = 'button';
+
+    backgroundModule.append(backgroundButton);
+    document.body.insertAdjacentElement('beforeend', backgroundModule);
+  }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,17 @@
 export function random(min, max) {
-    return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
 
 export function createScope() {
-    const scope = document.createElement('div')
-    scope.id = 'scope'
-    document.body.append(scope)
+  const scope = document.createElement('div');
+  scope.id = 'scope';
+  document.body.append(scope);
+}
+
+export function randomColor() {
+  const randomColorStr = [random(0, 255), random(0, 255), random(0, 255)].join(
+    ',',
+  );
+  const randomColor = `rgb(${randomColorStr})`;
+  return randomColor;
 }

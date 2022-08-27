8b507a7f8948906665bb2b3b927d66314c6b793b
LiliyaShaih <shayhelis@yandex.ru>
2022-08-27 13:56:20 +0300

    improved code style

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,30 +1,33 @@
 import { Module } from '../core/module';
-import { random } from '../utils';
+import { randomColor } from '../utils';
 
 export class BackgroundModule extends Module {
   constructor(type, text) {
     super(type, text);
   }
 
-  getRandomColor() {
-    console.log('hello');
-    const randomColorStr = [
-      random(0, 255),
-      random(0, 255),
-      random(0, 255),
-    ].join(',');
-    const randomColor = `rgb(${randomColorStr})`;
-    document.body.style.backgroundColor = randomColor;
-
-    const colorName = document.querySelector('.background-module__color');
-    colorName.textContent = randomColor;
-  }
-
   trigger() {
+    this.createHtml();
     const backgroundModule = document.querySelector('.background-module');
     backgroundModule.classList.add('open');
 
     const colorBtn = document.querySelector('.background-module__btn');
     colorBtn.addEventListener('click', this.getRandomColor);
+  }
+
+  getRandomColor() {
+    const backgroundColor = randomColor();
+    document.body.style.backgroundColor = backgroundColor;
+
+    const colorName = document.querySelector('.background-module__color');
+    colorName.textContent = backgroundColor;
+  }
+
+  createHtml() {
+    const backgroundHtml = `<div class="background-module">
+    <button class="background-module__btn" type="button">Сменить фон</button>
+    <p class="background-module__color"><span>Цвет в RGB</span></p>
+  </div>`;
+    document.body.insertAdjacentHTML('beforeend', backgroundHtml);
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,14 @@ export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
 
+export function randomColor() {
+  const randomColorStr = [random(0, 255), random(0, 255), random(0, 255)].join(
+    ',',
+  );
+  const randomColor = `rgb(${randomColorStr})`;
+  return randomColor;
+}
+
 // create scope
 export function createScope() {
   const scope = document.createElement('div');

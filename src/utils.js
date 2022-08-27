5a5af31e7a2996559b23153bea8440ea9a6a6d2d
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-08-27 17:56:51 +0700

    Implemented Context Menu

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,6 @@ export function random(min, max) {
     return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
 
-// create scope
 export function createScope() {
     const scope = document.createElement('div')
     scope.id = 'scope'

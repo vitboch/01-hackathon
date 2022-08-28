5c4eb6a30b5dc1c0417f258a0cbf9c0c28c37034
LiliyaShaih <shayhelis@yandex.ru>
2022-08-28 13:42:12 +0300

    conflicts resolve

--- src/utils.js
+++ src/utils.js
@@ -31,3 +31,36 @@ export function createModal(modalId) {
 export function getScope(id = '') {
   return document.querySelector(id);
 }
+
+export function randomColor() {
+  const randomColorStr = [random(0, 255), random(0, 255), random(0, 255)].join(
+    ',',
+  );
+  return `rgb(${randomColorStr})`;
+}
+
+export function getRandomColor() {
+  const firstColor = randomColor();
+  const secondColor = randomColor();
+  document.body.style.background = `linear-gradient(${firstColor},${secondColor})`;
+}
+
+export function createBackgroundModuleHtml() {
+  const backgroundModule = document.createElement('div');
+  backgroundModule.classList.add('background-module');
+
+  const backgroundButton = document.createElement('button');
+  backgroundButton.classList.add('background-module__btn');
+  backgroundButton.classList.add('background-module__btn--change');
+  backgroundButton.textContent = 'Change background';
+  backgroundButton.type = 'button';
+
+  const backgroundCloseButton = document.createElement('button');
+  backgroundCloseButton.classList.add('background-module__btn');
+  backgroundCloseButton.classList.add('background-module__btn--close');
+  backgroundCloseButton.textContent = 'Close';
+  backgroundCloseButton.type = 'button';
+
+  backgroundModule.append(backgroundButton, backgroundCloseButton);
+  document.body.insertAdjacentElement('beforeend', backgroundModule);
+}

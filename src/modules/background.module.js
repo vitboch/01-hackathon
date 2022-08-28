cd957fa2f544cbfe8422503d30a7e7e5fbacab86
LiliyaShaih <shayhelis@yandex.ru>
2022-08-28 16:44:00 +0300

    feature update-buttons transition

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,23 +1,29 @@
-import { Module } from '../core/module';
-import { getRandomColor, createBackgroundModuleHtml } from '../utils';
+import { Module } from '../core/module'
+import { getRandomColor, createBackgroundModuleHtml } from '../utils'
 
 export class BackgroundModule extends Module {
   constructor(type, text) {
-    super(type, text);
+    super(type, text)
   }
 
   trigger() {
-    createBackgroundModuleHtml();
-    const backgroundModule = document.querySelector('.background-module');
-    backgroundModule.classList.add('open');
+    createBackgroundModuleHtml()
+    const backgroundModule = document.querySelector('.background-module')
+    backgroundModule.classList.add('open')
 
-    const colorBtn = document.querySelector('.background-module__btn--change');
-    colorBtn.addEventListener('click', getRandomColor);
+    const colorBtn = document.querySelector('.background-module__btn--change')
+    colorBtn.addEventListener('click', getRandomColor)
 
-    const closeBtn = document.querySelector('.background-module__btn--close');
+    const closeBtn = document.querySelector('.background-module__btn--close')
     closeBtn.addEventListener('click', () => {
-      backgroundModule.remove();
-      document.body.style.background = 'inherit';
-    });
+      backgroundModule.remove()
+      document.body.style.background = 'inherit'
+    })
+
+    const contextMenu = document.querySelector('.menu')
+    contextMenu.addEventListener('click', () => {
+      backgroundModule.style.top = '15%'
+      backgroundModule.style.left = '10%'
+    })
   }
 }

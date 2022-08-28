8074d29cda0475de3ab416fb76ed260b4a40c60d
unknown <kamalutdinova@mail.ru>
2022-08-28 07:56:33 +0300

    Implemented shape module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
 import './styles.css'
+import {createScope} from '@/utils'
+import {ContextMenu} from '@/menu'
+import {BackgroundModule} from '@/modules/background.module'
+import {ClicksModule} from '@/modules/clicks.module'
+import {ShapeModule} from '@/modules/shape.module'
 
+const contextMenu = new ContextMenu('.menu')
+const backgroundModule = new BackgroundModule('#', 'Background module')
+const clicksModule = new ClicksModule('#', 'Clicks module')
+const shapeModule = new ShapeModule('#', 'Shape module')
+
+createScope()
+const scope = document.querySelector('#scope')
+scope.addEventListener('contextmenu', e => contextMenu.open(e))
+document.body.addEventListener('click', e => contextMenu.close(e))
+
+contextMenu.add(backgroundModule)
+contextMenu.add(clicksModule)
+contextMenu.add(shapeModule)
--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,17 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
+import { createCanvas } from '../utils'
+import { drawShape } from '../utils'
+import { removeElement } from '../utils'
 
 export class ShapeModule extends Module {
+  constructor(type, text) {
+    super(type, text)
+  }
 
+  trigger() {
+    removeElement('canvas')
+    const canvas = createCanvas()
+    canvas.classList.add('open')
+    drawShape()
+  }
 }
